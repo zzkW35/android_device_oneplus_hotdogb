5aa5d8ab887ab792bbaa7e0099f76f25b7b9e03c
Filippo Roggi <zzkw35@gmail.com>
2020-05-10 23:24:17 +0200

    hotdog: Adapt to StatiXOS
    * With this first commit I am simply adapting the LineageOS hotdog (OP7T Pro) tree to StatiXOS.
    
    * The commits that follow this one are the ones which will be responsible for the adaptation of the tree to hotdogb (OP7T).
    
    Change-Id: Ide48b715ee81da402bb945e74dda12ecd926e467

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2020 The StatiXOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,10 +13,7 @@ $(call inherit-product, vendor/oneplus/hotdog/hotdog-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-custom
 
 # A/B
 AB_OTA_PARTITIONS += \
