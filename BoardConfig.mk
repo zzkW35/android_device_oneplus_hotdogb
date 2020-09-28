46a21075494483bb3cb6c30b9792f2f5c2a78bcf
Filippo <zzkw35@gmail.com>
2020-09-28 08:08:42 +0200

    hotdogb: hotdog --> hotdogb
    * Simply change to hotdogb (OP7T) all the text where hotdog (OP7TPro) gets mentioned.
    
    * Set fingerprint to OOS 10.0.9.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,10 +6,10 @@
 -include device/oneplus/sm8150-common/BoardConfigCommon.mk
 
 BOARD_VENDOR := oneplus
-DEVICE_PATH := device/oneplus/hotdog
+DEVICE_PATH := device/oneplus/hotdogb
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := OnePlus7TPro
+TARGET_OTA_ASSERT_DEVICE := OnePlus7T
 
 # Display
 TARGET_SCREEN_DENSITY := 560
--- carbon_hotdogb.mk
+++ carbon_hotdogb.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from hotdog device
-$(call inherit-product, device/oneplus/hotdog/device.mk)
+# Inherit from hotdogb device
+$(call inherit-product, device/oneplus/hotdogb/device.mk)
 
 # Inherit Carbon GSM telephony parts
 $(call inherit-product, vendor/carbon/config/gsm.mk)
@@ -18,13 +18,12 @@ $(call inherit-product, vendor/carbon/config/gsm.mk)
 $(call inherit-product, vendor/carbon/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := carbon_hotdog
-PRODUCT_DEVICE := hotdog
+PRODUCT_NAME := carbon_hotdogb
+PRODUCT_DEVICE := hotdogb
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 7T Pro
+PRODUCT_MODEL := OnePlus 7T
 PRODUCT_MANUFACTURER := OnePlus
 
-PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
@@ -33,10 +32,10 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7TPro_EEA/OnePlus7TPro:10/QKQ1.190716.003/1910120055:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus7T_EEA/OnePlus7T:10/QKQ1.190716.003/2003271900:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=OnePlus7TPro \
-    PRODUCT_NAME=OnePlus7TPro \
-    PRIVATE_BUILD_DESC="OnePlus7TPro_EEA-user 10 QKQ1.190716.003 1910120055 release-keys"
+    PRODUCT_DEVICE=OnePlus7T \
+    PRODUCT_NAME=OnePlus7T \
+    PRIVATE_BUILD_DESC="OnePlus7T_EEA-user 10 QKQ1.190716.003 2003271900 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
